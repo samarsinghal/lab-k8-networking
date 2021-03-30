21daee6904397634ae7f26a53727669125411fe9
samar singhal <samarsinghal@ssamar-a01.vmware.com>
2021-03-29 21:28:58 -0600

    Updated Content

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst < ../k8-networking/helloworld-ingress.yaml.in > ../k8-networking/helloworld-ingress.yaml
+envsubst < k8-networking/helloworld-ingress.yaml.in > k8-networking/helloworld-ingress.yaml
 
 # envsubst < frontend-v3/ingress.yaml.in > frontend-v3/ingress.yaml
 # envsubst < frontend-v4/ingress.yaml.in > frontend-v4/ingress.yaml
